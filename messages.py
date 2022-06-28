39fedcb77b7fd09f03d82da28a8b2789a8cd2a04
Francesca Lanzi <francesca.lanzi6@studio.unibo.it>
2022-06-28 14:56:56 +0200

    Write function for "put" option for the filetransfer

--- messages.py
+++ messages.py
@@ -1,10 +1,26 @@
-import socket
+# 0000970828 Lanzi Francesca
+# Questo file contiene le funzioni
+# utili al server e al client
+
+import socket, sys, os
 
 def getFromServer():
     return
 
-def putOnServer():
+def putOnServer(filename, socket):
+    file = open(filename, 'rb')
+    file_data = file.read(1024)
+    socket.send(file_data)
+    print("\n\r File sent successufully!")
+    return
+
+def getOnServer(filename, socket):
+    file = open(filename, 'wb')
+    file_data = socket.recv(1024)
+    file.write(file_data)
+    file.close()
+    print("\n\r File received successefully!")
     return
 
 def listFromServer():
-    return
+    return str(os.listdir(os.getcwd())).encode()
