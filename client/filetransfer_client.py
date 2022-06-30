5a06c741fd7de88a6f769de16b888afb245cd3e4
Francesca Lanzi <francesca.lanzi6@studio.unibo.it>
2022-06-30 18:25:53 +0200

    Complete the filetransfer files methods

--- client/filetransfer_client.py
+++ client/filetransfer_client.py
@@ -1,33 +1,48 @@
 import socket, sys, time
+sys.path.append('/home/francesca/progetto_reti')
+import messages as mes
 
-BUFFSIZE = 4096
-PORT = 10000
+BUFFSIZE = int(4096)
+PORT = int(10000)
 
 c_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ('', PORT)
-print('\n\r Choose between this commands: \n\r list of files \
-    \n\r put <<file>> on server \n\r get <<file>> from server \
-    \n\r exit the program')
+server_address = ('localhost', PORT)
+print('\n\r Choose between this commands: \n\r list: list of the files in the server \
+    \n\r put <<file>>: puts a file on server \n\r get <<file>>: gets a file from server \
+    \n\r exit: exit the program')
 
 try: 
     while True:
         message = input('\n\r Insert command: ')
-        request = message.split()
-        print('\n\r Sending %s to server' % message)
-        sent = c_sock.send(message.encode(), server_address)
+        #print('\n\r Sending %s to server' % message)
+        c_sock.sendto(message.encode('utf8') , server_address)
         print('\n\r Waiting to receive... ')
-        data, server = c_sock.recvfrom(BUFFSIZE)
-        line = data.decode('utf8').split()
+        list_message = message.split()
 
-        if request[0] == 'list':
+        if list_message[0] == 'list':
             print('\n\r Here is the list of files: ')
-            for i in line:
+            data, server = c_sock.recvfrom(BUFFSIZE)
+            line = data.decode('utf-8').split()
+            i = 0
+            for i in line[i]:
                 print('\n\r %s' % i)
             
-        if request[0] == 'put':
-            filename = request[1]
-        #if line == 'get':
-        if line == 'exit':
+        if list_message[0] == 'put':
+            filename = line[1]
+            c_sock.sendto(filename, server_address)
+            print("\n\r Sending %s..." % filename)
+            mes.sendFile(filename, c_sock, server_address, BUFFSIZE)
+            print("\n\r File sent correctly")
+            c_sock.recvfrom(BUFFSIZE)
+
+        if list_message[0] == 'get':
+            filename = line[1]
+            c_sock.sendto(filename, server_address)
+            print("\n\r Sending %s request..." % filename)
+            mes.receiveFile(filename, c_sock, BUFFSIZE)
+            print("\n\r File received successfully.")
+        
+        if message == 'exit':
             exit()
 
 except Exception as info:
--- server/filetransfer_server.py
+++ server/filetransfer_server.py
@@ -1,23 +1,40 @@
-import socket, time
+import socket, time, sys
+sys.path.append('/home/francesca/progetto_reti')
 import messages as mes
 
-BUFFSIZE = 4096
-PORT = 10000
+BUFFSIZE = int(4096)
+PORT = int(10000)
 
 s_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ('', PORT)
+server_address = ('localhost', PORT)
 print('\n\r Waiting up on %s, port %s' % server_address)
 s_sock.bind(server_address)
 
 while True:
     print('\n\r Waiting to receive a message... ')
-    data, c_address = s_sock.recvfrom(BUFFSIZE)
+    data, client = s_sock.recvfrom(BUFFSIZE)
     #print('\n\r Received %s bytes from port %s' % (len(data), address))
-    line = data.decode('utf8')
+    line = data.decode('utf8').split()
+    print(data)
     
-    if line == 'list':
-        s_sock.sendto(mes.listFromServer(), c_address)
+    if line[0] == 'list':
+        s_sock.sendto(mes.listFromServer(), client)
+        print("\n\r List sent")
 
-    if line == 'exit':
+    if line[0] == 'put':
+        filename = line[1]
+        while True:
+            mes.receiveFile(filename, s_sock, BUFFSIZE)
+            break
+        ok = "\n\rFile received correctly"
+        s_sock.sendto(ok.encode('utf8'), server_address)
+
+    if line[0] == 'get':
+        filename = line[1]
+        s_sock.sendto(filename, server_address)
+        print("\n\r Sending %s..." % filename)
+        mes.sendFile(filename, s_sock, server_address, BUFFSIZE)
+
+    if line[0] == 'exit':
         exit()
 
